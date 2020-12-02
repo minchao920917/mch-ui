11ce2769467ab027b36232e744c815d1dd201f1b
minchao920917 <499790879@qq.com>
2020-12-02 23:38:51 +0800

    btn

--- packages/btn/src/btn.vue
+++ packages/btn/src/btn.vue
@@ -8,13 +8,6 @@
 <script>
 export default {
   name: 'McBtn', // 这个名字很重要
-  data () {
-    return {
-      
-    }
-  },
-  methods: {
-  }
 }
 </script>
 
@@ -31,4 +24,46 @@ export default {
   color: white;
   cursor: pointer;
 }
+
+/*
+* Button Sizes
+*
+* This file creates the various button sizes
+* (ex. .button-large, .button-small, etc.)
+*/
+.button-giant {
+  font-size: 28px;
+  height: 70px;
+  line-height: 70px;
+  padding: 0 70px; }
+
+.button-jumbo {
+  font-size: 24px;
+  height: 60px;
+  line-height: 60px;
+  padding: 0 60px; }
+
+.button-large {
+  font-size: 20px;
+  height: 50px;
+  line-height: 50px;
+  padding: 0 50px; }
+
+.button-normal {
+  font-size: 16px;
+  height: 40px;
+  line-height: 40px;
+  padding: 0 40px; }
+
+.button-small {
+  font-size: 12px;
+  height: 30px;
+  line-height: 30px;
+  padding: 0 30px; }
+
+.button-tiny {
+  font-size: 9.6px;
+  height: 24px;
+  line-height: 24px;
+  padding: 0 24px; }
 </style>
