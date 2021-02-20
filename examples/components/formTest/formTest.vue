25251cc3de030468caad5166ee4d07a66673410e
minchao920917 <499790879@qq.com>
2021-02-20 16:01:51 +0800

    输入框

--- examples/components/formTest/formTest.vue
+++ examples/components/formTest/formTest.vue
@@ -1,95 +1,138 @@
 <template>
   <div>
+    <h3>mc输入框</h3>
+    <hr />
+    <mc-input
+      v-model="input1"
+      type="text"
+      autocomplete="off"
+      placeholder="请输入内容"
+    ></mc-input>
+    <mc-input
+      v-model="input2"
+      type="text"
+      :disabled="true"
+      autocomplete="off"
+      placeholder="不能输入"
+    ></mc-input>
+    <mc-input
+      v-model="input3"
+      type="text"
+      :clearable="true"
+      autocomplete="off"
+      placeholder="带清楚按钮的输入"
+    ></mc-input>
+    <mc-input
+      v-model="input4"
+      type="number"
+      autocomplete="off"
+      placeholder="请输入number"
+    ></mc-input>
+    <mc-input
+      v-model="input5"
+      type="textarea"
+      autocomplete="off"
+      placeholder="内容输入框"
+    ></mc-input>
     <h3>mcForm表单</h3>
-    <hr>
+    <hr />
     <mc-form :model="ruleForm" :rules="rules" ref="mcForm">
       <mc-form-item prop="age" label="年龄">
-        <mc-input v-model="ruleForm.age" type="text"></mc-input>
+        <mc-input
+          v-model="ruleForm.age"
+          type="text"
+          autocomplete="new-password"
+          placeholder="请输入年龄"
+        ></mc-input>
       </mc-form-item>
       <mc-form-item prop="pass" label="密码">
-        <mc-input v-model="ruleForm.pass" type="password"></mc-input>
+        <mc-input
+          v-model="ruleForm.pass"
+          type="password"
+          autocomplete="new-password"
+        ></mc-input>
       </mc-form-item>
       <mc-form-item>
         <mc-btn type="primary" @click="submitForm('mcForm')">提交</mc-btn>
       </mc-form-item>
     </mc-form>
-    
   </div>
 </template>
 
 <script>
-
-  export default {
-
-    data() {
-      var checkAge = (rule, value, callback) => {
-        if (!value) {
-          return callback(new Error('年龄不能为空'));
-        }
-        setTimeout(() => {
-          if (!Number.isInteger(value)) {
-            callback(new Error('请输入数字值'));
-          } else {
-            if (value < 18) {
-              callback(new Error('必须年满18岁'));
-            } else {
-              callback();
-            }
-          }
-        }, 1000);
-      };
-      var validatePass = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请输入密码'));
-        } else {
-          if (this.ruleForm.checkPass !== '') {
-            this.$refs.ruleForm.validateField('checkPass');
-          }
-          callback();
-        }
-      };
-      var validatePass2 = (rule, value, callback) => {
-        if (value === '') {
-          callback(new Error('请再次输入密码'));
-        } else if (value !== this.ruleForm.pass) {
-          callback(new Error('两次输入密码不一致!'));
-        } else {
-          callback();
-        }
-      };
-      return {
-        ruleForm: {
-          pass: '',
-          checkPass: '',
-          age: ''
-        },
-        rules: {
-          pass: [
-            { validator: validatePass, trigger: 'blur' }
-          ],
-          checkPass: [
-            { validator: validatePass2, trigger: 'blur' }
-          ],
-          age: [
-            { validator: checkAge, trigger: 'blur' }
-          ]
-        }
-      };
-    },
-    methods: {
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
+export default {
+  data() {
+    var checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("年龄不能为空"));
       }
-    }
-  }
+      setTimeout(() => {
+        if (!Number.isInteger(value)) {
+          callback(new Error("请输入数字值"));
+        } else {
+          if (value < 18) {
+            callback(new Error("必须年满18岁"));
+          } else {
+            callback();
+          }
+        }
+      }, 1000);
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.ruleForm.checkPass !== "") {
+          this.$refs.ruleForm.validateField("checkPass");
+        }
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.ruleForm.pass) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      input: "",
+      input1: "",
+      input2: "",
+      input3: "",
+      input4: "",
+      input5: "",
+      input6: "",
+      input7: "",
+      input8: "",
+      ruleForm: {
+        pass: "",
+        checkPass: "",
+        age: "",
+      },
+      rules: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        checkPass: [{ validator: validatePass2, trigger: "blur" }],
+        age: [{ validator: checkAge, trigger: "blur" }],
+      },
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
 </script>
--- packages/input/src/input.vue
+++ packages/input/src/input.vue
@@ -1,6 +1,48 @@
 <template>
-  <div>
-    <input @input="onInput" :value="value" v-bind="$attrs" />
+  <div
+    :class="[
+      type === 'textarea' ? 'mc-textarea' : 'mc-input',
+      {
+        'is-disabled': inputDisabled,
+        'mc-input--prefix': $slots.prefix,
+        'mc-input--suffix': $slots.suffix,
+      },
+    ]"
+  >
+    <!-- 前置元素 -->
+    <div class="mc-input-prefix" v-if="$slots.prefix">
+      <slot name="prefix"></slot>
+    </div>
+
+    <input
+      v-if="type !== 'textarea'"
+      class="mc-input_inner"
+      @input="onInput"
+      :value="value"
+      :disabled="inputDisabled"
+      :type="type"
+      v-bind="$attrs"
+    />
+    <textarea
+      v-else
+      class="mc-textarea_inner"
+      @input="onInput"
+      ref="textarea"
+      v-bind="$attrs"
+      :disabled="inputDisabled"
+    >
+    </textarea>
+    <mc-icon
+      class="mc-input_clear"
+      v-if="showClear"
+      :icon="'delete'"
+      @click="clear"
+    ></mc-icon>
+    <!-- @mousedown.prevent -->
+    <!-- 后置元素 -->
+    <div class="mc-input-suffix" v-if="$slots.suffix">
+      <slot name="suffix"></slot>
+    </div>
   </div>
 </template>
 
@@ -9,9 +51,29 @@ export default {
   name: "McInput",
   inheritAttrs: false, //关闭属性传递
   props: {
+    type: {
+      type: String,
+      default: "text",
+    },
     value: {
       type: String,
       default: "",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    clearable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    inputDisabled() {
+      return this.disabled;
+    },
+    showClear() {
+      return this.clearable && !this.disabled;
     },
   },
   methods: {
@@ -19,6 +81,84 @@ export default {
       this.$emit("input", e.target.value);
       this.$parent.$emit("validate");
     },
+    clear() {
+      console.log("value", this.value);
+      this.$emit("input", "");
+      // this.$emit("change", "");
+      // this.$emit("clear");
+    },
   },
 };
 </script>
+<style lang="scss" scoped>
+@import "./../../scss/global.scss";
+.mc-input {
+  position: relative;
+  font-size: 14px;
+  display: inline-block;
+  width: 100%;
+  .mc-input_inner {
+    background-color: $white;
+    background-image: none;
+    border-radius: 4px;
+    border: 1px solid $borderColor;
+    box-sizing: border-box;
+    color: $wordColor;
+    display: inline-block;
+    font-size: inherit;
+    height: 40px;
+    line-height: 1;
+    outline: none;
+    padding: 0 15px;
+    transition: border-color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+    width: 100%;
+    &:focus {
+      outline: none;
+      border-color: $default;
+    }
+  }
+}
+.mc-input.is-disabled {
+  .mc-input_inner {
+    background-color: $disabledBgColor;
+    border-color: $disabledBdColor;
+    color: $disabledWdColor;
+    cursor: not-allowed;
+  }
+}
+
+/** textarea样式 */
+.mc-textarea {
+  display: inline-block;
+  width: 100%;
+  vertical-align: bottom;
+  font-size: 14px;
+  .mc-textarea_inner {
+    display: block;
+    resize: vertical;
+    padding: 5px 15px;
+    line-height: 1.5;
+    box-sizing: border-box;
+    width: 100%;
+    font-size: inherit;
+    color: $wordColor;
+    background-color: $white;
+    background-image: none;
+    border: 1px solid $borderColor;
+    border-radius: 4px;
+    transition: border-color 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+    &:focus {
+      outline: none;
+      border-color: $default;
+    }
+  }
+}
+.mc-textarea.is-disabled {
+  .mc-textarea_inner {
+    background-color: $disabledBgColor;
+    border-color: $disabledBdColor;
+    color: $disabledWdColor;
+    cursor: not-allowed;
+  }
+}
+</style>
